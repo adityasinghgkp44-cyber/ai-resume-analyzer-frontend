512e6aa5930c31e3654d0c95a5e4fc5c117c5b86
Aditya Kumar Singh <adityasinghgkp44@gmail.com>
2026-07-22 13:10:57 +0530

    Add dashboard debug logs

--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -1,47 +1,19 @@
 import "./StatCard.css";
 
-import CountUp from "react-countup";
+function StatCard({ title, value, icon, color }) {
+  return (
+    <div
+      className="stat-card"
+      style={{ borderTop: `4px solid ${color}` }}
+    >
+      <div className="stat-header">
+        <span>{title}</span>
+        {icon}
+      </div>
 
-function StatCard({
-    title,
-    value,
-    icon,
-    color
-}) {
-
-    return (
-
-        <div
-            className="stat-card"
-            style={{
-                borderTop:`4px solid ${color}`
-            }}
-        >
-
-            <div className="stat-header">
-
-                <span>{title}</span>
-
-                {icon}
-
-            </div>
-
-            <h2>
-
-                <CountUp
-
-                    end={value}
-
-                    duration={2}
-
-                />
-
-            </h2>
-
-        </div>
-
-    );
-
+      <h2>{value}</h2>
+    </div>
+  );
 }
 
 export default StatCard;
