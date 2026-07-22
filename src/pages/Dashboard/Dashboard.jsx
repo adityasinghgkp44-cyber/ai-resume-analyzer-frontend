2009e48571df738157763ee09f5980004e5b916c
Aditya Kumar Singh <adityasinghgkp44@gmail.com>
2026-07-22 13:07:00 +0530

    Add dashboard debug logs

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,18 @@
 import DashboardLayout from "../../layouts/DashboardLayout";
+import StatCard from "../../components/StatCard/StatCard";
+import { Target } from "lucide-react";
 
 function Dashboard() {
   return (
     <DashboardLayout>
-      <h1>Dashboard Working</h1>
+      <StatCard
+        title="Average ATS"
+        value={0}
+        color="#ff6a00"
+        icon={<Target />}
+      />
     </DashboardLayout>
   );
 }
-<DashboardLayout>
-  <StatCard
-    title="Average ATS"
-    value={0}
-    color="#ff6a00"
-    icon={<Target />}
-  />
-</DashboardLayout>
+
 export default Dashboard;
- 
